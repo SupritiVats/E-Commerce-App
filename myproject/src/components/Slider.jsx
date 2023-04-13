d4fbf430124b481161747724dac7b1082233bcc5
Supriti Vats <supritivats123@gmail.com>
2023-04-13 20:51:56 +0530

    commit

--- myproject/src/components/Slider.jsx
+++ myproject/src/components/Slider.jsx
@@ -1,77 +1,120 @@
-import { ArrowLeftOutlined, ArrowRightOutlined } from '@material-ui/icons'
-import React from 'react'
-import styled from 'styled-components'
+import { ArrowLeftOutlined, ArrowRightOutlined } from "@material-ui/icons";
+import React from "react";
+import styled from "styled-components";
+//import winter from "../Assests/winter.jpg";
+import { sliderItems } from "../data";
+import { useState } from "react";
 
-const Container= styled.div`
-width:100%;
-height:100vh;
-display: flex;
-position: relative;
-`
-const Arrow=styled.div`
-width:50px;
-height:50px;
-background-color: #fff7f7;
-border-radius:50%;
-display: flex;
-align-items: center;
-justify-content: center;
-position:absolute;
-top:0;
-bottom:0;
-left: ${props=> props.direction==="left" && "10px"};
-right: ${props=> props.direction==="right" && "10px"};
-cursor:pointer;
-opacity:0.5;
-margin: auto;
+const Container = styled.div`
+padding-top: 10px;
+  width: 100%;
+  height: 90vh;
+  display: flex;
+  position: relative;
+  overflow: hidden;
+`;
+const Arrow = styled.div`
+  width: 50px;
+  height: 50px;
+  background-color: #fff7f7;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: ${(props) => props.direction === "left" && "10px"};
+  right: ${(props) => props.direction === "right" && "10px"};
+  cursor: pointer;
+  opacity: 0.5;
+  margin: auto;
+  z-index: 2;
+`;
+const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+  transition: all 1.5s ease;
+  transform: translateX(${(props) => props.slideIndex * -100}vw);
+`;
+const Slide = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+  background-color: #${(props) => props.bg};
+`;
 
-`
-const Wrapper= styled.div`
-   height:100%;
+const ImageContainer = styled.div`
+  flex: 1;
+  height: 100%;
+`;
 
-`
- const Slide= styled.div`
-           display: flex;
-           align-items: center;
-           width: 100vw;
-           height: 100vh;
- `
+const Image = styled.img`
+  height: 100%;
+ 
+`;
 
-
- const ImageContainer= styled.div
-`flex: 1;
-height: 100%`
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 50px;
+`;
 
 
-const  Image= styled.img`
-height: 80%`
+const Title = styled.h1`
+  font-size: 70px;
+`;
 
+const Desc = styled.p`
+  margin: 50px 0px;
+  font-size: 20px;
+  font-weight: 500;
+  letter-spacing: 3px;
+`;
 
+const Button = styled.button`
+  padding: 10px;
+  font-size: 20px;
+  background-color: transparent;
+  cursor: pointer;
+`;
 
-
-const InfoContainer= styled.div
-`flex: 1;
-padding: 50 px`
 const Slider = () => {
+
+    const [slideIndex, setSlideIndex] = useState(0);
+  const handleClick = (direction) => {
+    if (direction === "left") {
+      setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2);
+    } else {
+      setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0);
+    }
+  };
+
+
   return (
     <Container>
-             <Arrow direction="left">
-                 <ArrowLeftOutlined/>
-             </Arrow>
-             <Wrapper>
-                 <Slide>
-                 <ImageContainer>
-                    <Image src="E-Commerce-App\myproject\src\Assests\WhatsApp Image 2023-03-04 at 18.44.06.jpeg" alt="no"></Image>
-                 </ImageContainer>
-                 <InfoContainer/>
-                 </Slide>
-             </Wrapper>
-             <Arrow direction="right">
-
-                 <ArrowRightOutlined/>
-             </Arrow>
+      <Arrow direction="left" onClick={() => handleClick("left")}>
+        <ArrowLeftOutlined />
+      </Arrow>
+      <Wrapper slideIndex={slideIndex}>
+        {sliderItems.map((item) => (
+          <Slide bg={item.bg} key={item.id}>
+            <ImageContainer>
+              <Image src={item.img} />
+            </ImageContainer>
+            <InfoContainer>
+              <Title>{item.title}</Title>
+              <Desc>{item.desc}</Desc>
+              <Button>SHOW NOW</Button>
+            </InfoContainer>
+          </Slide>
+        ))}
+      </Wrapper>
+      <Arrow direction="right" onClick={() => handleClick("right")}>
+        <ArrowRightOutlined />
+      </Arrow>
     </Container>
-    )
-}
+  );
+};
 
-export default Slider
+export default Slider;
---
+++ myproject/src/data.js
@@ -0,0 +1,26 @@
+import autum from './Assests/autum.jpg'
+import winter from './Assests/winter.jpg'
+import sports from './Assests/sports.jpg'
+export const sliderItems = [
+    {
+      id: 1,
+      img: winter,
+      title: "WINTER COLLECTION",
+      desc: "FLAT 50% OFF on EACH PICK! GET IT NOW. WHY TO WAIT TILL LAST?",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: autum,
+      title: "AUTUMN COLLECTION",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: sports,
+      title: "GET SPORT ON STYLE",
+      desc: "LOOK CLASSY WITH NEW TRENDING STYLE AND START YOUR OWN FASHION.",
+      bg: "fbf0f4",
+    },
+  ];
