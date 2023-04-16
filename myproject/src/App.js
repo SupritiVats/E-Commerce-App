7c6e2a4140de412e1ff4506bc3cf0e281749af53
Supriti Vats <supritivats123@gmail.com>
2023-04-16 15:56:57 +0530

    commit

--- myproject/src/App.js
+++ myproject/src/App.js
@@ -1,8 +1,14 @@
-import Home from './pages/Home';
+// import Home from './pages/Home';
+// import Product from './pages/Product';
+import Register from './pages/Register';
+// import ProductList from './pages/ProductList';
 
 function App() {
   return (
-    <Home/>
+    // <Home/>
+    // <Product/>
+    // <ProductList/>
+    <Register/>
   );
 }
 
